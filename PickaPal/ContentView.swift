30cb70a38f00c0950e99e16a0d0bbb0caccb495a
sangwook16074 <qazws78941@gmail.com>
2025-08-06 11:50:49 +0900

    Initial Commit

---
+++ PickaPal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PickaPal
+//
+//  Created by 한상욱 on 8/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PickaPal/PickaPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickaPalApp.swift
+//  PickaPal
+//
+//  Created by 한상욱 on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct PickaPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
